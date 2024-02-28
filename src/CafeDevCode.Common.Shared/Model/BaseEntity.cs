f0cba94736c124b98042aa959bb55f0b99a49e80
halac123b <halac123b@gmail.com>
2024-02-28 16:13:32 +0700

    Update database

--- src/CafeDevCode.Common.Shared/Model/BaseEntity.cs
+++ src/CafeDevCode.Common.Shared/Model/BaseEntity.cs
@@ -15,7 +15,7 @@ namespace CafeDevCode.Common.Shared.Model
         public DateTime? CreatedAt { get; set; }
         public string? LastUpdatedBy { get;set; }
         public DateTime? LastUpdatedAt { get; set; }
-        public bool? IsDeleted { get; set; }
+        public bool IsDeleted { get; set; }
 
         public BaseEntity SetCreateInfo(string user, DateTime date) {
             CreatedBy = user;
--- src/CafeDevCode.Database/Entities/Category.cs
+++ src/CafeDevCode.Database/Entities/Category.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CafeDevCode.Database.Entities
 {
-    internal class Categoru
+    internal class Category
     {
     }
 }
